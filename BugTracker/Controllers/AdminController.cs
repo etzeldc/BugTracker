226e5ee9b0f0be48e28c905dfa47df4db93e0e38
etzeldc <etzeldc@gmail.com>
2019-08-01 17:30:37 -0400

    Initial commit

---
+++ BugTracker/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using BugTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BugTracker.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET Admin
+        public ActionResult UserIndex()
+        {
+            var users = db.Users.Select(u => new UserProfileViewModel
+            {
+                Id = u.Id,
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                DisplayName = u.DisplayName,
+                AvatarUrl = u.AvartarUrl,
+                Email = u.Email
+            }).ToList();
+
+            return View(users);
+        }
+        //public ActionResult ManageAssignments()
+        //{
+
+        //}
+
+    }
+}
