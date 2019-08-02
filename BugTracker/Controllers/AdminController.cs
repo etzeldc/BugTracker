42a536b000f76339bc24231c91d2bd7f71cc87bf
etzeldc <etzeldc@gmail.com>
2019-08-02 16:50:08 -0400

    Manage ALL User Roles
    Combined UserIndex and Manage User Roles into one table with multiselection.

--- BugTracker/Controllers/AdminController.cs
+++ BugTracker/Controllers/AdminController.cs
@@ -14,7 +14,7 @@ namespace BugTracker.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
         private UserRolesHelper roleHelper = new UserRolesHelper();
         // GET Admin
-        public ActionResult UserIndex()
+        public ActionResult ManageUserRoles()
         {
             var users = db.Users.Select(u => new UserProfileViewModel
             {
@@ -25,43 +25,79 @@ namespace BugTracker.Controllers
                 AvatarUrl = u.AvartarUrl,
                 Email = u.Email
             }).ToList();
-
+            ViewBag.RoleName = new SelectList(db.Roles.ToList(), "Name", "Name");
             return View(users);
         }
 
         //
         // Get
 
-        public ActionResult ManageUserRole(string userId)
-        {
-            //Loading a DropDownList with Role information??
-            //New SelectList("data pushed into control", 
-            //               "column used to push selection into control",
-            //               "column we show the user inside control",
-            //               "if role is occupied, show that instead of nothing")
-            var currentRole = roleHelper.ListUserRoles(userId).FirstOrDefault();
-            ViewBag.UserId = userId;
-            ViewBag.Roles = new SelectList(db.Roles.ToList(), "Name", "Name", currentRole);
-            return View();
-        }
+        //public ActionResult ManageUserRole(string userId)
+        //{
+        //    //Loading a DropDownList with Role information??
+        //    //New SelectList("data pushed into control", 
+        //    //               "column used to push selection into control",
+        //    //               "column we show the user inside control",
+        //    //               "if role is occupied, show that instead of nothing")
+        //    var currentRole = roleHelper.ListUserRoles(userId).FirstOrDefault();
+        //    ViewBag.UserId = userId;
+        //    ViewBag.Roles = new SelectList(db.Roles.ToList(), "Name", "Name", currentRole);
+        //    return View();
+        //}
 
         //
         //// POST
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult ManageUserRole(string userId, string roles)
-        {
-            foreach (var role in roleHelper.ListUserRoles(userId))
-            {
-                roleHelper.RemoveUserFromRole(userId, role);
-            }
-            //If user 
-            if (!string.IsNullOrEmpty(roles))
-            {
-                roleHelper.AddUserToRole(userId, roles);
-            }
-            return RedirectToAction("UserIndex");
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult ManageUserRole(string userId, string userRole)
+        //{
+        //    foreach (var role in roleHelper.ListUserRoles(userId))
+        //    {
+        //        roleHelper.RemoveUserFromRole(userId, userRole);
+        //    }
+        //    //If user 
+        //    if (!string.IsNullOrEmpty(userRole))
+        //    {
+        //        roleHelper.AddUserToRole(userId, userRole);
+        //    }
+        //    return RedirectToAction("UserIndex");
+        //}
 
+        // GET
+        //public ActionResult ManageRoles()
+        //{
+        //    var users = db.Users.Select(u => new UserProfileViewModel
+        //    {
+        //        Id = u.Id
+        //    }).ToList();
+        //    ViewBag.Users = new MultiSelectList(db.Users.ToList(), "Id", "DisplayName"); //Look into that Full Name thing
+        //    ViewBag.RoleName = new SelectList(db.Roles.ToList(), "Name", "Name");
+        //    return View(users);
+        //}
+
+        [HttpPost]
+        public ActionResult ManageUserRoles(string userId, string roleName)
+        {
+            //go through list of users'
+            //if (users != null)
+            //{
+
+            //foreach (var userId in users)
+            //{
+                //remove all roles
+                foreach (var role in roleHelper.ListUserRoles(userId))
+                {
+                    roleHelper.RemoveUserFromRole(userId, role);
+                }
+                if (!string.IsNullOrEmpty(roleName))
+                {
+
+                //add new role
+                roleHelper.AddUserToRole(userId, roleName);
+                }
+            //}
+            //}
+            return RedirectToAction("ManageUserRoles");
+        }
     }
 }
