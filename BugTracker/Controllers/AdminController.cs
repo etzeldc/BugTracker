8aea2aa1047a325d24f0072eedf34e8456b3e766
etzeldc <etzeldc@gmail.com>
2019-08-02 13:43:26 -0400

    Assigning Roles, Scripts/Styles, And Organization
    Included user role management functions, stylized tables, and organized shared layout

--- BugTracker/Controllers/AdminController.cs
+++ BugTracker/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using BugTracker.Models;
+﻿using BugTracker.Helpers;
+using BugTracker.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,10 +8,11 @@ using System.Web.Mvc;
 
 namespace BugTracker.Controllers
 {
-    [Authorize(Roles = "Admin")]
+    //[Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
+        private UserRolesHelper roleHelper = new UserRolesHelper();
         // GET Admin
         public ActionResult UserIndex()
         {
@@ -26,10 +28,40 @@ namespace BugTracker.Controllers
 
             return View(users);
         }
-        //public ActionResult ManageAssignments()
-        //{
 
-        //}
+        //
+        // Get
+
+        public ActionResult ManageUserRole(string userId)
+        {
+            //Loading a DropDownList with Role information??
+            //New SelectList("data pushed into control", 
+            //               "column used to push selection into control",
+            //               "column we show the user inside control",
+            //               "if role is occupied, show that instead of nothing")
+            var currentRole = roleHelper.ListUserRoles(userId).FirstOrDefault();
+            ViewBag.UserId = userId;
+            ViewBag.Roles = new SelectList(db.Roles.ToList(), "Name", "Name", currentRole);
+            return View();
+        }
+
+        //
+        //// POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ManageUserRole(string userId, string roles)
+        {
+            foreach (var role in roleHelper.ListUserRoles(userId))
+            {
+                roleHelper.RemoveUserFromRole(userId, role);
+            }
+            //If user 
+            if (!string.IsNullOrEmpty(roles))
+            {
+                roleHelper.AddUserToRole(userId, roles);
+            }
+            return RedirectToAction("UserIndex");
+        }
 
     }
 }
